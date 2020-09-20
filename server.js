3073358c80fbd8a5e88ccbfacafaa7186c2527c5
Ruzgar Ata Ozkan <ruzgarata6@gmail.com>
2020-09-21 00:05:45 +0300

    serverside improvements

--- server.js
+++ server.js
@@ -2,12 +2,41 @@
 
 const { useEncryption } = require('./src/hooks/useEncryption');
 const express = require('express');
+const terminalImage = require('terminal-image');
+const figlet = require('figlet');
 const app = express();
 const { encryptPassword, decryptPassword } = useEncryption();
 
-app.get('/', (req, res) => res.send('Hello World'));
+const data = {
+    "unicorn": 5,
+    "doom": 2,
+    "gloom": -1
+};
 
-app.listen(3000, () => console.log('Server has started!'));
+app.listen(3000, async () => {
+    console.log(await terminalImage.file('./images/WITHOUT_WARNING.png', { width: '100%' }));
+    console.log(figlet.textSync('WITHOUT', {
+        font: 'big',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: false
+    }));
+    console.log(figlet.textSync('WARNING', {
+        font: 'big',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: false
+    }));
+    console.log(figlet.textSync('uitwaaien . . .', {
+        font: 'big',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: false
+    }));
+});
 
 app.use(express.static('./src/screens'));
 app.use(express.json());
@@ -22,8 +51,24 @@ app.post('/', (req, res) => {
     const { encryptedPassword, encryptedCoefficient, coefficients } = req.body;
     console.log(` # Encryption: ${encryptedPassword} - ${encryptedCoefficient}`);
     const decryptedPassword = handleEncryption(encryptedPassword, encryptedCoefficient, coefficients);
-    // do something with the decryptedPassword
-    // then send the encryptedPasswords to client again
-    // and thats how you communicate safely between client side and serverside...
+    /* 
+        do whatever you want with the decryptedPassword
+        then send the encryptedPasswords to client again
+        and thats how you communicate safely between client side and serverside... 
+    */
     res.json({ encryptedPassword, encryptedCoefficient, coefficients });
 });
+
+app.get('/all', (req, res) => {
+    res.send(data);
+});
+
+app.get('/add/:word/:score', addWord);
+
+function addWord(req, res) {
+    const params = req.params;
+    data[params.word] = parseInt(params.score);
+    res.send({
+        "message": "Thank you for your contribution"
+    });
+}
--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://6a6b09edd18c.ngrok.io'
+    baseURL: `https://${1}.ngrok.io`,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 });
